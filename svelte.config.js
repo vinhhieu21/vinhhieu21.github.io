ad91e9bfbdf0eb9625322b8685e48c449ce9cd3f
Hieu Bui Vinh <buivinhhieu217@gmail.com>
2026-03-22 23:34:40 +0700

    Add SvelteKit website runtime files

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build'
+		})
+	},
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) =>
+			filename.includes('node_modules') ? undefined : { runes: true }
+	}
+};
+
+export default config;
